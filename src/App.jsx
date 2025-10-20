25fb15781e8bcfaf4fc1e327f48aeae6ec12569e
Krayker01 <107270701+Krayker01@users.noreply.github.com>
2025-10-20 17:48:06 -0400

    changed the data to dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import './App.css';
 import Footer from './components/Footer.jsx';
 import HeaderName from "./components/Header.jsx";
 import Note from './components/Note.jsx';
+import notes from './components/notes.js';
 
 function App() {
+
+  const newNotes = notes.map(x => <Note
+    key={x.key}
+    title={x.title}
+    content={x.content}
+  />);
+
   return (
     <div className="app-container">
       <HeaderName className="oswald-400 headerBackgroundClassName" />
-      <Note classForNoteDivs="classForNoteDivs" />
+      {newNotes}
       <Footer className="footerClassName" />
     </div>
   )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,12 @@
-function Note({ classForNoteDivs }) {
+function Note(props) {
     return (
         <div style={{ flex: 1 }}>
-            <div className={classForNoteDivs}>
-                <h2 className="bbh-sans-hegarty-regular">This is the Note title</h2>
-                <p>This is the Note content</p>
+            <div className="classForNoteDivs">
+                <h2 className="bbh-sans-hegarty-regular">{props.title}</h2>
+                <p>{props.content}</p>
             </div>
         </div>
-    )
+    );
 }
 
 export default Note;
