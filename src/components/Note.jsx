ceca885eac10aa7424e6636c10cea435c4c8cf21
Krayker01 <107270701+Krayker01@users.noreply.github.com>
2025-10-20 20:49:06 -0400

    some bugs have been fixed

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,8 @@
 function Note(props) {
     return (
-        <div style={{ flex: 1 }}>
-            <div className="classForNoteDivs">
-                <h2 className="bbh-sans-hegarty-regular">{props.title}</h2>
-                <p>{props.content}</p>
-            </div>
+        <div className="classForNoteDivs">
+            <h2 className="bbh-sans-hegarty-regular">{props.title}</h2>
+            <p>{props.content}</p>
         </div>
     );
 }
